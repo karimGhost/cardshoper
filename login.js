64339f56602e1877ad58675163e17c807c587f84
karimGhost <74273577+karimGhost@users.noreply.github.com>
2022-04-07 04:01:35 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+function cacel(){
+
+    document.getElementById('open').style.display='none';
+
+    document.getElementById('ope').style.display = 'block';
+}
+
+
+
+
+function closeIt(){
+    document.getElementById('open').style.display=  'block';
+
+    document.getElementById('ope').style.display = 'none';
+  
+}
+
+   

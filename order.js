286d5a834ee34011c730c690a5845372b0382f7c
karimGhost <74273577+karimGhost@users.noreply.github.com>
2022-05-11 07:57:22 +0300

    Update order.js

--- order.js
+++ order.js
@@ -1,53 +1,87 @@
 window.addEventListener("DOMContentLoaded", function(e){
 
-    const ordered = localStorage.getItem('orderbtn');
+    
+    let ordered = localStorage.getItem('items');
 
-   let order = JSON.parse(ordered);
-
-  
-
-    if(ordered){
-       
-
-        const Box = document.querySelector('.items');
-
-
-const detai = Box.querySelector('.item_detail');
-
-
-
-
-const price = Box.querySelector('.item_price');
-
-
-
-//const cart_text = Box.querySelector('.cart_text');
-//const cart_button = Box.querySelector('.cart_button');
-
+   let order =  JSON.parse(ordered)
  
-    detai.innerText = order.title;
-price.innerText = order.pric ;
+   for(let i = 0; i < order.length; i++){
+   
+   
+    let goo = [];
+ 
+   
+    
+     
+ 
 
-const pric = document.querySelector('.total').innerText = order.pric;
+  let it = document.querySelector("#table");
 
+  let table = '';
+
+  table +=  `<tr>
+  <th class="p">cart</th>
+  <th>amount</th>
+  <th>price</th>
+  <th class="price"> <i class="fa fa-shopping-cart"></i></th>
   
+</tr> ` ;
 
-//price.innerText = order.pri;
+  JSON.parse(localStorage.getItem('items')).map(data=>{
 
-//cart_text.innerText = order.cart_text;
-//cart_button.innerText = order.cart_btn;
+   
+
+    table += ` 
+    <tr>
+      <td data-vi=${data.name} style="cursor:pointer" #="go"  class=" va item_detail">${data.name}</td> 
+      <td  data-vi=${data.name} ><input id="go" data-vi=${data.name}  class="va io" value="1"; min="1" max="100" type="number"></td>
+      <td class=" item_price" style="display: none">  ${data.price} </td>
+  <td class="fo total">${data.price}</td>
+      </tr>
+    
+
+   
+    `
+
+
+
+
+
+})  
+ 
+  it.innerHTML = table;
+let q = document.querySelector(".item_price").innerText;
+document.querySelector(".total").innerText = q;
+
+   }
+
+let vlk = document.querySelector("#table");
+
+let o = document.querySelector(".vl")
+.addEventListener("click", function(e){
+ 
+  let va = e.querySelector(".item_price");
+
+  console.log(va)
+
+
+
+
+})
+
+
 
 
 const img = Box.querySelector('.pic');
-img.setAttribute('src', `image/${order.id}.png`);
+img.setAttribute('src', `${order.id}.png`);
 
 
 img.setAttribute('alt', order.title)
 
 
 
-}
+
 
 
     
-});
+})

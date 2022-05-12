0936e04b45d78765de114df46fd0b567637a2bd7
karimGhost <74273577+karimGhost@users.noreply.github.com>
2022-05-12 05:29:38 +0300

    Update card.js

--- card.js
+++ card.js
@@ -32,12 +32,12 @@ window.addEventListener("DOMContentLoaded", function(e){
     
     
     items.push(item)
+                              let count = document.querySelector(".cart_count").innerHTML =  items.length ;
 
       //  localStorage.setItem("items", JSON.stringify(items))
     
          confirm(`do you want to add '${item.name}  ${item.price}' to the cart `) ? localStorage.setItem('items',JSON.stringify(items)) + alert(`${item.price  + " " +   item.name} successfully added to cart`) + item.circle.classList.add('dow') + item.circle.classList.add('clss') + setTimeout(disp, 2000)  + item.off + count  : alert("Card Is discarded !!!")  + item.pr 
 
-                              let count = document.querySelector(".cart_count").innerHTML =  items.length ;
 
     
     if(JSON.parse(localStorage.getItem("items")) === null){
